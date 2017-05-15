8924ae076914a0c9a6e52d7d1aa520b47298c4ad
Shiyi Chen <sychen@udel.edu>
2017-05-15 17:50:57 -0400

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello to only you");
+    //return String.format("Hello, %s!", someone);
   }
 }
